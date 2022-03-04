0ceb71d9a571483b3f99bf1a27cac6faf79c56d9
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-03-04 15:01:26 +0000

    Commit three scraping

--- scraping.py
+++ scraping.py
@@ -11,33 +11,34 @@ for data in soup.find_all('div', class_='mqs-prop-dt-wrapper'):
     inner_li_bed = data.find('li', class_='bed')                  # bed tag data
     inner_li_garage = data.find('li',class_='garage')             # garage tag data
     price_data = data.find('p',class_='h3')                       # price data tag
-
+    loc_data = data.find('h2')
     # checking if bathroom data is available before scraping number
     if inner_li_shower is not None:
         bathrooms = inner_li_shower.span.text
     else:
         bathrooms = None
 
-        # checking if bed data is available before scraping number
-        if inner_li_bed is not None:
-            bedrooms = inner_li_bed.span.text
-        else:
-            bedrooms = None
+    # checking if bed data is available before scraping number
+    if inner_li_bed is not None:
+        bedrooms = inner_li_bed.span.text
+    else:
+        bedrooms = None
 
      # checking if garage data is available before scraping number
     if inner_li_garage is not None:
         garage = inner_li_garage.span.text
     else:
         garage = None
-#
+
+    # slicing actual price from scraped price data
     if price_data is not None:
         price = price_data.text
     else:
         price = None
 
-#     # slicing actual location from scraped location data
-    if data.h2.a is not None:
-        loc = data.h2.a.text.split()               # converts location data to list of items
+    # slicing actual location from scraped location data
+    if loc_data is not None:
+        loc = loc_data.a.text.split()                          # converts location data to list of items
         location = " ".join(loc[loc.index('at') + 1:None])    # takes name from list by slicing list
     else:
         location = None
@@ -51,4 +52,4 @@ for data in soup.find_all('div', class_='mqs-prop-dt-wrapper'):
 print(count)
 # loc = '4 bedroom House for sale at Adenta Frafraha'.split()
 # print(" ".join(loc[loc.index('at')+1:None]))
-print(soup.prettify())
+# print(soup.prettify())
