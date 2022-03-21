32f0ae8277a5f493283c6de0ad214c71622d130e
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-03-21 01:22:37 +0000

    Commit two on scraping using selenium

--- scraping.py
+++ scraping.py
@@ -17,38 +17,38 @@ count = 1
 
 try:
     datacontainer = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,"listview")))
-    databox = datacontainer.find_elements_by_class_name("row mqs-featured-prop-inner-wrap clickable")
+    databox = datacontainer.find_element(By.CLASS_NAME("row.mqs-featured-prop-inner-wrap.clickable"))
 
 # source = requests.get("https://meqasa.com/houses-for-sale-in-Accra.html?w=4").text
 # soup = BeautifulSoup(source, 'lxml')
 # # Scraping all html tags with data needed
     for data in databox:
-        inner_li_shower = data.find_elements_by_class_name('shower')             # bathroom tag data
-        inner_li_bed = data.find_elements_by_class_name('bed')                  # bed tag data
-        inner_li_garage = data.find_elements_by_class_name('garage')             # garage tag data
-        price_data = data.find_elements_by_class_name('h3')                       # price data tag
-        loc_data = data.find_elements_by_class_name('h2')
+        inner_li_shower = data.find_element(By.CLASS_NAME('shower'))       # bathroom tag data
+        inner_li_bed = data.find_element(By.CLASS_NAME('bed'))                # bed tag data
+        inner_li_garage = data.find_element(By.CLASS_NAME('garage'))            # garage tag data
+        price_data = data.find_element(By.CLASS_NAME('h3')).text                 # price data tag
+        loc_data = data.find_element(By.CLASS_NAME('h2')).text
         # checking if bathroom data is available before scraping number
         if inner_li_shower is not None:
-            bathrooms = inner_li_shower.span.text
+            bathrooms = inner_li_shower.find_element(By.TAG_NAME("span")).text
         else:
             bathrooms = None
 
         # checking if bed data is available before scraping number
         if inner_li_bed is not None:
-            bedrooms = inner_li_bed.span.text
+            bedrooms = inner_li_bed.find_element(By.TAG_NAME("span")).text
         else:
             bedrooms = None
 
          # checking if garage data is available before scraping number
         if inner_li_garage is not None:
-            garage = inner_li_garage.span.text
+            garage = inner_li_garage.find_element(By.TAG_NAME("span")).getText()
         else:
             garage = None
 
         # slicing actual price from scraped price data
         if price_data is not None:
-            main_price = "".join(price_data.text[9:].split(',')).strip()         # Splitting price from string
+            main_price = "".join(price_data[9:].split(',')).strip()         # Splitting price from string
             # checks if splitted price is a number or string
             if main_price.isdigit():
                 price = int(main_price)
@@ -59,7 +59,7 @@ try:
 
         # slicing actual location from scraped location data
         if loc_data is not None:
-            loc = loc_data.a.text.split()                          # converts location data to list of items
+            loc = loc_data.findElement(By.TAG_NAME("a")).text                      # converts location data to list of items
             location = " ".join(loc[loc.index('at') + 1:None])    # takes name from list by slicing list
         else:
             location = None
