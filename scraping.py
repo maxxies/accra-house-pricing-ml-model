a9df757193077700282621808050ce7cdce8b160
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-03-25 02:43:15 +0000

    Updates on selenium scraping multiple pages

--- scraping.py
+++ scraping.py
@@ -6,74 +6,87 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+import time
 
-
+# Driver settings
 PATH = "C:\programs\chromedriver.exe"
 ser = Service(PATH)
 op = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=ser, options= op)
+driver.get("https://meqasa.com/houses-for-sale-in-Accra.html?w=1")     # Url of page to be scrapped
 
-driver.get("https://meqasa.com/houses-for-sale-in-Accra.html?w=1")
 count = 1
 
+# Scraping data
 try:
-    datacontainer = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "listview")))
-    databox = datacontainer.find_elements(By.CLASS_NAME, "row.mqs-featured-prop-inner-wrap.clickable")
+    page_count = 0
+    while page_count < 3:
+        # Scrapes container from which data is found.(container--> HTML element data is in)
+        datacontainer = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "listview")))
+        # Gets all the data from the container
+        databox = datacontainer.find_elements(By.CLASS_NAME, "row.mqs-featured-prop-inner-wrap.clickable")
 
-# source = requests.get("https://meqasa.com/houses-for-sale-in-Accra.html?w=4").text
-# soup = BeautifulSoup(source, 'lxml')
-# # Scraping all html tags with data needed
-#     for data in databox:
-    data = databox[7]
-    inner_li_shower = data.find_element(By.CLASS_NAME,'shower').get_attribute("innerHTML")     # bathroom tag data
-    inner_li_bed = data.find_element(By.CLASS_NAME,'bed').get_attribute("innerHTML")                # bed tag data
-    inner_li_garage = data.find_element(By.CLASS_NAME,'garage').get_attribute("innerHTML")            # garage tag data
-    price_data = data.find_element(By.CLASS_NAME,'h3').getText()           # price data tag
-    loc_data = data.find_element(By.TAG_NAME,'h2').getText()
-    print(inner_li_shower)
-    print(inner_li_bed)
-    print(inner_li_garage)
-    print(price_data)
-    print(loc_data)
+        #   Loops through list of data to scrap specific data-information from a data taken at a time
+        for data in databox:
+            # Scraping the number of shower from a single data
+            try:
+                li_shower = data.find_element(By.CLASS_NAME,'shower')
+                inner_li_shower = li_shower.find_element(By.TAG_NAME,'span').get_attribute("innerHTML")
+            except NoSuchElementException:
+                # Sets none when no shower-data is found for a particular data
+                inner_li_shower = None
 
+            # Scraping he number of bed spaces give data
+            try:
+                li_bed = data.find_element(By.CLASS_NAME,'bed')
+                inner_li_bed = li_bed.find_element(By.TAG_NAME,'span').get_attribute("innerHTML")
+            except NoSuchElementException:
+                # Sets none when no bed-data is found for a particular data
+                inner_li_bed = None
 
+            # Scraping number of garage
+            try:
+                li_garage = data.find_element(By.CLASS_NAME,'garage')
+                inner_li_garage = li_garage.find_element(By.TAG_NAME,'span').get_attribute("innerHTML")
+            except NoSuchElementException:
+                # Sets none when no garage-data is found for a particular data
+                inner_li_garage = None
 
-    #     # checking if bathroom data is available before scraping number
-    #
-    # bathrooms = inner_li_shower.find_element(By.TAG_NAME,"span").text
-    #
-    #     #
-    #     # # checking if bed data is available before scraping number
-    #
-    # bedrooms = inner_li_bed.find_element(By.TAG_NAME,"span").text
-    #
-    #     #
-    #     #  # checking if garage data is available before scraping number
-    #
-    # garage = inner_li_garage.find_element(By.TAG_NAME,"span").text
-    #
-    #
-    #     # # slicing actual price from scraped price data
-    #     # if price_data is not None:
-    # main_price = "".join(price_data[9:].split(',')).strip()         # Splitting price from string
-    #     # checks if splitted price is a number or string
-    # if main_price.isdigit():
-    #     price = int(main_price)
-    # else:
-    #     price = None
-    #
-    #     # # slicing actual location from scraped location data
-    #
-    # loc = loc_data.findElement(By.TAG_NAME,"a").text                      # converts location data to list of items
-    # location = " ".join(loc[loc.index('at') + 1:None])    # takes name from list by slicing list
-    #
-    # print("----------------------------------->>>",count)
-    # print("Location :", location)
-    # print('bathrooms :', bathrooms)
-    # print('bedrooms :', bedrooms)
-    # print('garage :', garage)
-    # print('Price :',price)
-    # count += 1
+            # Scraping price
+            price_data = data.find_element(By.XPATH,"//p[@class='h3']").get_attribute("innerText")
+
+            # Scraping location
+            loc_box = data.find_element(By.TAG_NAME,'h2')
+            loc_data = loc_box.find_element(By.TAG_NAME,'a').get_attribute("innerHTML")
+
+            # Splitting price from string
+
+            price_data = str(price_data)
+            main_price = "".join(price_data[3:].split(',')).strip()
+                # checks if splitted price is a number or string
+            if main_price.isdigit():
+                price = int(main_price)
+            else:
+                price = None
+
+            # slicing actual location from scraped location data
+            location = "".join(loc_data[loc_data.index('at') + 2:None])    # takes name from list by slicing list
+
+            print("----------------------------------->>>", count)
+            print("Location :", location)
+            print("Shower :",inner_li_shower)
+            print("Bed :",inner_li_bed)
+            print("Garage :",inner_li_garage)
+            print('Price :',price)
+            count += 1
+
+        count = 1
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "pagenumnext")))
+        button.click()
+        page_count = page_count + 1
+        time.sleep(10)
+
 except TimeoutException :
     print("TIme out error.")
     driver.close()
