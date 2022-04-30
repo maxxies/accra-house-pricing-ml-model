3523c3b923d085ce26ea519eb45ae26b761b73a5
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-04-30 01:26:46 +0000

    Adding fast api deployment script

--- deployment/app_fastapi.py
+++ deployment/app_fastapi.py
@@ -1,13 +1,19 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 import math
-from fastapi import FASTAPI
 from geopy.geocoders import Nominatim
-import joblib
 import pickle
 import warnings
 import numpy as np
 
-app = FASTAPI()
-# model = joblib.load('model.joblib')
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 model = pickle.load(open("model_lr.pkl", "rb"))
 
 
@@ -17,69 +23,63 @@ def price_predictor(to_predict_list):
     return result[0]
 
 
-@app.route('/', methods=['GET'])
-def start_app():
+@app.get('/', response_class=HTMLResponse)
+def start_app(request :Request):
     prediction = None
     error = None
 
-    return render_template('index.html', prediction=prediction, error=error)
+    return templates.TemplateResponse("index.html", {"request":Request, "prediction":prediction, "error":error})
 
 
-@app.route('/', methods=['POST'])
-def predict():
+@app.post('/', response_class=HTMLResponse)
+def predict(request:Request, location: str = Form(...), bathrooms: int = Form(...), bedrooms: int = Form(...), garages: int = Form(...)):
     prediction = None
     error = None
 
-    if request.method == 'POST':
-        location = request.form['location']
-        bathroom = request.form['bathrooms']
-        bedroom = request.form['bedrooms']
-        garage = request.form['garages']
+    location = location
+    bathroom = bathrooms
+    bedroom = bedrooms
+    garage = garages
 
-        # cleans location input by user
-        if ',' in location:
-            end = location.index(',')
-        else:
-            end = None
-        location = "".join(location[: end])  # takes name from list by slicing
-        if location.lower() == "dome":  # To distinguish among Benin's and Ghana's
-            location = location + ',Ghana'
-
-        try:
-            # Gets longitude and latitude from location entered
-            geolocator = Nominatim(user_agent="maxmawube@gmail.com")
-            location_coordinates = geolocator.geocode(location)
-            latitude = location_coordinates.latitude
-            longitude = location_coordinates.longitude
-        except:
-            latitude = None
-            longitude = None
-
-
-        # checks if coordinates were received
-        if latitude is None and longitude is None:
-            return render_template('index.html', data=location, one=bathroom, two=bedroom, three=garage,
-                                   latitude=latitude, longitude=longitude, prediction=prediction,
-                                   error="Could not get coordinates of location, no or weak connection.")
-        # Checks if locations are found in Accra only
-        elif math.floor(latitude) != 5:
-            return render_template('index.html', data=location, one=bathroom, two=bedroom, three=garage,
-                                   latitude=latitude, longitude=longitude, prediction=prediction,
-                                   error="Location not found in Accra.")
-        # When no error is encountered
-        else:
-            # Making predictions : latitude,longitude, bedrooms, garage, bathroom
-            if garage == '':
-                garage = 0
-            predictprice = price_predictor([5.704139, -0.168796, int(bedroom), int(garage), int(bathroom)])
-            warnings.filterwarnings("ignore")
-            prediction = "{0:,.2f}".format(predictprice)
-
-            return render_template('index.html', prediction=prediction, error=error)
-
+    # cleans location input by user
+    if ',' in location:
+        end = location.index(',')
     else:
-        return render_template('index.html', prediction=prediction, error=error)
+        end = None
+    location = "".join(location[: end])  # takes name from list by slicing
+    if location.lower() == "dome":  # To distinguish among Benin's and Ghana's
+        location = location + ',Ghana'
+
+    try:
+        # Gets longitude and latitude from location entered
+        geolocator = Nominatim(user_agent="maxmawube@gmail.com")
+        location_coordinates = geolocator.geocode(location)
+        latitude = location_coordinates.latitude
+        longitude = location_coordinates.longitude
+    except:
+        latitude = None
+        longitude = None
+
+
+    # checks if coordinates were received
+    if latitude is None and longitude is None:
+        return templates.TemplateResponse('index.html', {"request":Request, "prediction":prediction,
+                                   "error":"Could not get coordinates of location, no or weak connection."})
+    # Checks if locations are found in Accra only
+    elif math.floor(latitude) != 5:
+        return templates.TemplateResponse('index.html',{"request":Request, "prediction":prediction,
+                                   "error":"Location not found in Accra."})
+    # When no error is encountered
+    else:
+        # Making predictions : latitude,longitude, bedrooms, garage, bathroom
+        if garage == '':
+            garage = 0
+        predictprice = price_predictor([5.704139, -0.168796, int(bedroom), int(garage), int(bathroom)])
+        warnings.filterwarnings("ignore")
+        prediction = "{0:,.2f}".format(predictprice)
+
+        return templates.TemplateResponse('index.html', {"request":Request, "prediction":prediction, "error":error})
 
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    uvicorn.run(app)
